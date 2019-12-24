af396db56bc8823257703cb1e401794e976c53e0
trevorleake <trevorjohnleake@gmail.com>
2019-12-24 11:39:19 -0800

    Move print statements to logs

--- craigslist_scraper.py
+++ craigslist_scraper.py
@@ -1,17 +1,10 @@
 from Scraper import Scraper
 import argparse
-from utils import BadOptionsError, convert_to_date
+from utils import BadOptionsError, convert_to_date, LogDecorator
+import logging
 
-'''
-This script is meant to be called with either { city and count } or a { url and bucket_dir }.
-City and count will pull count many ads from that city (note cities like New
-York must be type newyork for now). You can also grab a single ad using URL and
-post it to a specific bucket, specified by bucket_dir. Min word count is used to grab
-only longer ads from a city.
 
-e.g. python craigslist_scraper.py --city portland --count 60 --min-word-count 30
-'''
-
+@LogDecorator()
 def craigslist_scraper(url=None, destination_bucket_dir=None, city=None, count=None, min_word_count=None, date=None):
     ####################
     # VALIDATE OPTIONS #
@@ -21,11 +14,10 @@ def craigslist_scraper(url=None, destination_bucket_dir=None, city=None, count=N
         raise BadOptionsError('Must specify either URL or CITY from which to scrape')
 
     if url:
-        print('As a particular ad\'s URL is specified, all options COUNT, MIN_WORD_COUNT, CITY, and DATE are disregarded')
+        logging.warning('As a particular ad\'s URL is specified, all options COUNT, MIN_WORD_COUNT, CITY, and DATE are disregarded')
 
     if city and not count:
-        print('As COUNT is not specified, default of 100 ads will be used')
-
+        logging.warning('As COUNT is not specified, default of 100 ads will be used')
 
 
     s = Scraper()
@@ -43,7 +35,6 @@ def craigslist_scraper(url=None, destination_bucket_dir=None, city=None, count=N
         return count_scraped
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', help='URL of single craigslist missed connections ad to scrape')
@@ -56,4 +47,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     count_scraped = craigslist_scraper(**vars(args))
-    print(f'We scraped {count_scraped} ad(s)')
+    logging.info(f'We scraped {count_scraped} ad(s)')
